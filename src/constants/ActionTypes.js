1b66c6f5c200a37e758467eafb2c0e80a4aefc17
Yuriy Koziy <yukoziy@gmail.com>
2020-04-12 14:06:42 -0400

    Added SET_INTERVAL_ID action type

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -2,6 +2,7 @@ export const ADJUST_BREAK_LENGTH = 'ADJUST_BREAK_LENGTH';
 export const ADJUST_SESSION_LENGTH = 'ADJUST_SESSION_LENGTH';
 export const SWITCH_STATE = 'SWITCH_STATE';
 export const PRESS_RESET = 'PRESS_RESET';
+export const SET_INTERVAL_ID = 'SET_INTERVAL_ID';
 
 export const SWITCH_TYPE = 'SWITCH_TYPE';
 export const SET_TIME = 'SET_TIME';
