51538cca9676b035431610a8b9ab8142efb98749
Yuriy Koziy <yukoziy@gmail.com>
2020-04-12 12:29:18 -0400

    SessionPane is now connected to the redux store.

--- src/components/SessionPane.js
+++ src/components/SessionPane.js
@@ -1,16 +1,45 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { adjustSessionLength } from '../actions'
 
-const SessionPane = ({sessionLen, handleAdjustSession}) => {
+const SessionPane = ({sessionLen, currentState, adjustSessionLength}) => {
+
+    const onClickAdjust = (event) => {
+      if(currentState === "run") {
+          return;
+      }
+      let currId = event.currentTarget.id;
+      let currSessionLen = sessionLen;
+      if(currId.includes("increment") && (currSessionLen < 60)) {
+          currSessionLen += 1;
+      } else if(currId.includes("decrement") && (currSessionLen > 1)){
+          currSessionLen -= 1;
+      }
+      adjustSessionLength(currSessionLen);
+    }
+
     return(
         <div className="session-pane">
             <div id="session-label">Session Length</div>
             <div className="adjust">
-              <button id="session-decrement" onClick={handleAdjustSession}>-</button>
+              <button id="session-decrement" onClick={onClickAdjust}>-</button>
               <div id="session-length">{sessionLen}</div>
-              <button id="session-increment" onClick={handleAdjustSession}>+</button>
+              <button id="session-increment" onClick={onClickAdjust}>+</button>
             </div>
         </div>
     );
 }
 
-export default SessionPane;
+const mapStateToProps = (state) => ({
+    sessionLen: state.sessionLen,
+    currentState: state.currentState
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  adjustSessionLength: (sessionLen) => dispatch(adjustSessionLength(sessionLen))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SessionPane);
