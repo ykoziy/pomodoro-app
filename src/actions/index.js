e9949c9d2374ddf266fed1c7add3f43fbe15fadf
Yuriy Koziy <yukoziy@gmail.com>
2020-04-11 20:00:19 -0400

    Added Redux action

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import {
+  ADJUST_BREAK_LENGTH,
+  ADJUST_SESSION_LENGTH,
+  SWITCH_STATE,
+  PRESS_RESET,
+  SWITCH_TYPE,
+  SET_START_TIME
+} from "../constants/actionTypes";
+
+export const switchState = (state) => ({type: SWITCH_STATE, state});
