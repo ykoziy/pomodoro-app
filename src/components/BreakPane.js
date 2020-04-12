4fc3d4690c535001a58898691b83ed50f5c07236
Yuriy Koziy <yukoziy@gmail.com>
2020-04-12 11:45:51 -0400

    Connected BreakPane to the redux store.

--- src/components/BreakPane.js
+++ src/components/BreakPane.js
@@ -1,16 +1,45 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { adjustBreakLength } from '../actions'
 
-const BreakPane = ({breakLen, handleAdjustBreak}) => {
+const BreakPane = ({breakLen, currentState, adjustBreakLength}) => {
+  
+    const onClickAdjust = (event) => {
+      if(currentState === "run") {
+          return;
+      }
+      const currId = event.currentTarget.id;
+      let currbreakLen = breakLen;
+      if(currId.includes("increment") && (currbreakLen < 60)) {
+          currbreakLen += 1;
+      } else if(currId.includes("decrement") && (currbreakLen > 1)){
+          currbreakLen -= 1;
+      }
+      adjustBreakLength(currbreakLen);
+    }
+
     return(
         <div className="break-pane">
             <div id="break-label">Break Length</div>
             <div className="adjust">
-                <button id="break-decrement" onClick={handleAdjustBreak}>-</button>
+                <button id="break-decrement" onClick={onClickAdjust}>-</button>
                 <div id="break-length">{breakLen}</div>
-                <button id="break-increment"  onClick={handleAdjustBreak}>+</button>
+                <button id="break-increment"  onClick={onClickAdjust}>+</button>
             </div>
         </div>
     );
 }
 
-export default BreakPane;
+const mapStateToProps = (state) => ({
+    breakLen: state.breakLen,
+    currentState: state.currentState
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  adjustBreakLength: (breakLen) => dispatch(adjustBreakLength(breakLen))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(BreakPane);
