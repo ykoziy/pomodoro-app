320b7d9ae5dc70d7f6736fd8d25153f34e3f6aa4
Yuriy Koziy <yukoziy@gmail.com>
2020-04-14 14:38:22 -0400

    Update code to use withSessionType.

--- src/containers/BreakPane.js
+++ src/containers/BreakPane.js
@@ -1,51 +1,15 @@
-import React from "react";
 import { connect } from 'react-redux';
-import { adjustBreakLength } from '../actions'
+import { adjustBreakLength } from '../actions';
+import withSessionType from './withSessionType';
 
-const BreakPane = ({breakLen, currentState, adjustBreakLength}) => {
-  
-    const onClickAdjust = (event) => {
-      if(currentState === "run") {
-          return;
-      }
-      const currId = event.currentTarget.id;
-      let currbreakLen = breakLen;
-      if(currId.includes("increment") && (currbreakLen < 60)) {
-          currbreakLen += 1;
-      } else if(currId.includes("decrement") && (currbreakLen > 1)){
-          currbreakLen -= 1;
-      }
-      adjustBreakLength(currbreakLen);
-    }
-
-    const onInput = (event) => {
-      let { value, min, max } = event.target;
-      value = Math.max(Number(min), Math.min(Number(max), Number(value)));
-      adjustBreakLength(value);
-    }
-
-    return(
-        <div className="break-pane">
-            <div id="break-label">Break Length</div>
-            <div className="adjust">
-                <button id="break-decrement" onClick={onClickAdjust}>-</button>
-                <input id="break-length" type="number" size="2" min="1" max="60" value={breakLen} onChange={onInput}/>
-                <button id="break-increment"  onClick={onClickAdjust}>+</button>
-            </div>
-        </div>
-    );
-}
+const BreakPane = () => {}
 
 const mapStateToProps = (state) => ({
-    breakLen: state.breakLen,
-    currentState: state.currentState
+    timeDuration: state.breakLen
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  adjustBreakLength: (breakLen) => dispatch(adjustBreakLength(breakLen))
+  adjustTimeDuration: (breakLen) => dispatch(adjustBreakLength(breakLen))
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(BreakPane);
+export default connect(mapStateToProps, mapDispatchToProps)(withSessionType(BreakPane, "break"));
