a7829619c4a4e24effc6f50b9caf321b9d30363b
Yuriy Koziy <yukoziy@gmail.com>
2020-04-14 14:38:28 -0400

    Update code to use withSessionType.

--- src/containers/SessionPane.js
+++ src/containers/SessionPane.js
@@ -1,51 +1,15 @@
-import React from "react";
 import { connect } from 'react-redux';
-import { adjustSessionLength } from '../actions'
+import { adjustSessionLength } from '../actions';
+import withSessionType from './withSessionType';
 
-const SessionPane = ({sessionLen, currentState, adjustSessionLength}) => {
-
-    const onClickAdjust = (event) => {
-      if(currentState === "run") {
-          return;
-      }
-      let currId = event.currentTarget.id;
-      let currSessionLen = sessionLen;
-      if(currId.includes("increment") && (currSessionLen < 60)) {
-          currSessionLen += 1;
-      } else if(currId.includes("decrement") && (currSessionLen > 1)){
-          currSessionLen -= 1;
-      }
-      adjustSessionLength(currSessionLen);
-    }
-
-    const onInput = (event) => {
-      let { value, min, max } = event.target;
-      value = Math.max(Number(min), Math.min(Number(max), Number(value)));
-      adjustSessionLength(value);
-    }
-
-    return(
-        <div className="session-pane">
-            <div id="session-label">Session Length</div>
-            <div className="adjust">
-              <button id="session-decrement" onClick={onClickAdjust}>-</button>
-              <input id="session-length" type="number" size="2" min="1" max="60" value={sessionLen} onChange={onInput}/>
-              <button id="session-increment" onClick={onClickAdjust}>+</button>
-            </div>
-        </div>
-    );
-}
+const SessionPane = ({sessionLen, adjustSessionLength}) => {}
 
 const mapStateToProps = (state) => ({
-    sessionLen: state.sessionLen,
-    currentState: state.currentState
+    timeDuration: state.sessionLen
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  adjustSessionLength: (sessionLen) => dispatch(adjustSessionLength(sessionLen))
+  adjustTimeDuration: (sessionLen) => dispatch(adjustSessionLength(sessionLen))
 });
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(SessionPane);
+export default connect(mapStateToProps, mapDispatchToProps)(withSessionType(SessionPane, "session"));
