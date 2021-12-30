ca721b23c72656d790b6b3fe92aef9034470f587
Yuriy Koziy <yukoziy@gmail.com>
2021-12-30 16:40:07 -0500

    Updates

---
+++ precache-manifest.a97916577262fb9895e6ae27c06cfea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899716bd607a9105ae3956a3fd3b7480",
+    "url": "/pomodoro-app/index.html"
+  },
+  {
+    "revision": "daea8c990324ff68254a",
+    "url": "/pomodoro-app/static/css/main.05215bc5.chunk.css"
+  },
+  {
+    "revision": "e8db27221e7e07de7214",
+    "url": "/pomodoro-app/static/js/2.f9ec2a79.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pomodoro-app/static/js/2.f9ec2a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daea8c990324ff68254a",
+    "url": "/pomodoro-app/static/js/main.b12109af.chunk.js"
+  },
+  {
+    "revision": "d9abe88a7bb7f3f583d7",
+    "url": "/pomodoro-app/static/js/runtime-main.74a465c0.js"
+  }
+]);
